d576b705109b204870744ee4bed1766561379582
Sebastian Wyngaard <swyngaard@sumo>
2011-04-21 10:41:49 +0200

    Added Particle, Strand and Hair classes

--- cpu/hair.cpp
+++ cpu/hair.cpp
@@ -3,6 +3,8 @@
 
 namespace pilar
 {
+
+////////////////////////////// Particle Class //////////////////////////////////
 	Particle::Particle(float mass)
 	{
 		this->mass = mass;
@@ -22,7 +24,97 @@ namespace pilar
 	
 	void Particle::update(float dt)
 	{
+		dt++; //TODO
+	}
+
+////////////////////////////// Spring Class ////////////////////////////////////
+
+	Spring::Spring(Particle* particle1, Particle* particle2, float k, float length, float damping, SpringType type)
+	{
+		this->k = k;
+		this->length = length;
+		this->damping = damping;
+		
+		this->particle[0] = particle1;
+		this->particle[1] = particle2;
+		
+	}
+	
+	void Spring::update(float dt)
+	{
+		dt++; //TODO
+	}
+	
+////////////////////////////// Strand Class ////////////////////////////////////
+
+	Strand::Strand(int numParticles, float mass, float k, float length, Vector3f root=Vector3f())
+	{
+		//TODO
+	}
+	
+	void Strand::buildSprings()
+	{
+		//TODO
+	}
+	
+	void Strand::resetParticles()
+	{
+		for(int i = 0; i < numParticles; i++)
+		{
+			particle[i]->clearForces();
+		}
+	}
+	
+	void Strand::updateSprings(float dt)
+	{
+		//TODO
 		dt++;
+	}
+	
+	void Strand::updateParticles(float dt)
+	{
+		//TODO
+		dt++;
+	}
+	
+	void Strand::update(float dt)
+	{
+		//TODO
+		dt++;
+	}
+	
+	void Strand::release()
+	{
+		//TODO
+	}
+	
+	void Strand::applyForce(Vector3f force)
+	{
+		//TODO
+		force = Vector3f();
+	}
+	
+	void Strand::applyStrainLimiting(float dt)
+	{
+		//TODO
+		dt++;
+	}
+
+/////////////////////////// Hair Class /////////////////////////////////////////
+	
+	Hair::Hair(int numParticles, float mass, float k, float length, std::vector<Vector3f> &roots)
+	{
+		//TODO
+	}
+	
+	void Hair::update(float dt)
+	{
+		//TODO
+	}
+	
+	void Hair::release()
+	{
+		//TODO
 	}
 }
 
--- cpu/hair.h
+++ cpu/hair.h
@@ -3,6 +3,7 @@
 #define __HAIR_H__
 
 #include "tools.h"
+#include <vector>
 
 namespace pilar
 {
@@ -20,6 +21,65 @@ namespace pilar
 		void clearForces();
 		void applyForce(Vector3f force);	
 		void update(float dt);
+	};
+	
+	enum SpringType
+	{
+		EDGE,
+		BENDING,
+		TORSION,
+		EXTRA
+	};
+	
+	class Spring
+	{
+	private:
+		Particle** particle;
+		float k;
+		float length;
+		float damping;
+		SpringType type;
+		
+	public:
+		Spring(Particle* particle1, Particle* particle2, float k, float length, float damping, SpringType type);
+		void update(float dt);
+	};
+	
+	class Strand
+	{
+	private:
+		int numParticles;
+		int numSprings;
+		
+		Vector3f root;
+		
+		Particle** particle;
+		Spring** spring;
+		
+		void buildSprings();
+		void resetParticles();
+		void updateSprings(float dt);
+		void updateParticles(float dt);
+		
+	public:
+		Strand(int numParticles, float mass, float k, float length, Vector3f root);
+		void update(float dt);
+		void release();
+		void applyForce(Vector3f force);
+		void applyStrainLimiting(float dt);
+	};
+	
+	class Hair
+	{
+	private:
+		Strand** strand;
+		int numStrands;
+		Vector3f gravity;
+		
+	public:
+		Hair(int numParticles, float mass, float k, float length, std::vector<Vector3f> &roots);
+		void update(float dt);
+		void release();
 	};
 }
 
